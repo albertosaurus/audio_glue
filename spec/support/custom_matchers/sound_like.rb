763587aa621a637f11f13829a6d86ae1ba73f637
Potapov Sergey <blake131313@gmail.com>
2013-09-21 17:32:56 +0300

    Do not show sox warnings in sound_like matcher
    Task 12148

--- spec/support/custom_matchers/sound_like.rb
+++ spec/support/custom_matchers/sound_like.rb
@@ -6,14 +6,14 @@
 #   "/Children_of_Bodom.mp3".should_not sound_like "/Britney_Spears.mp3"
 RSpec::Matchers.define :sound_like do |expected_file|
   match do |file|
-    rate      = 44100
+    rate      = 96000
     channels  = 1
     threshold = 0.95
 
     if File.exists?(expected_file) && File.exists?(file)
       # Convert input files into raw 16-bit signed audio (WAV) to process with Chromaprint
-      expected_audio = %x"sox #{expected_file} -e signed -b 16 -t wav - rate #{rate} channels #{channels}"
-      audio =          %x"sox #{file}          -e signed -b 16 -t wav - rate #{rate} channels #{channels}"
+      expected_audio = %x"sox #{expected_file} -e signed -b 16 -t wav - rate #{rate} channels #{channels} 2> /dev/null"
+      audio =          %x"sox #{file}          -e signed -b 16 -t wav - rate #{rate} channels #{channels} 2> /dev/null"
 
       # Get audio fingerprints
       chromaprint = Chromaprint::Context.new(rate, channels)
