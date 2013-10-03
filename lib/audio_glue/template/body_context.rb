554ede17f44550055fdbc7d2750ffc1671bc6f2e
Potapov Sergey <blake131313@gmail.com>
2013-10-03 15:25:00 +0300

    Documentation for BodyContext
    Task 12148

--- lib/audio_glue/template/body_context.rb
+++ lib/audio_glue/template/body_context.rb
@@ -1,6 +1,9 @@
 module AudioGlue
   class Template
+    # Context in which  +body+ statement of +.glue+ template is executed.
     class BodyContext
+      # @param packet [AudioGlue::Snippet] snippet packet where snippets will be added
+      # @param variables [Hash] hash to set instance variables
       def initialize(packet, variables = {})
         @__packet__ = packet
 
@@ -9,11 +12,20 @@ module AudioGlue
         end
       end
 
-
+      # Create snippet for +:file+ type.
+      #
+      # @param  file_path [String] path to an audio file in local file system
+      #
+      # @return [AudioGlue::Snippet]
       def file(file_path)
         Snippet.new(:file, file_path, @__packet__)
       end
 
+      # Create snippet for +:url+ type.
+      #
+      # @param remote_url [String] remote location of audio file
+      #
+      # @return [AudioGlue::Snippet]
       def url(remote_url)
         Snippet.new(:url, remote_url, @__packet__)
       end
