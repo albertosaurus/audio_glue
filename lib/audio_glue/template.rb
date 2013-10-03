1c5794aa1fbfd42f4548ba77f8b8fb47b1f3da88
Potapov Sergey <blake131313@gmail.com>
2013-10-03 13:57:39 +0300

    Add tests and documentation for Tepmlate
    Task 12148

--- lib/audio_glue/template.rb
+++ lib/audio_glue/template.rb
@@ -1,35 +1,56 @@
 module AudioGlue
   class Template
     extend Forwardable
-    def_delegators 'self.class', :format, :rate, :channels, :body_proc
+    def_delegators 'self.class', :format, :rate, :channels, :path, :body_proc
 
     class << self
       attr_accessor :format, :rate, :channels, :path, :body_proc
+    end
 
-      def head(&block)
-        HeadContext.new(self).instance_eval(&block)
-      end
+    # Method to process +head+ statement in +.glue+ templates.
+    #
+    # @yield block which will be executed in context of
+    #   {AudioGlue::Template::HeadContext} object.
+    #
+    # @return [void]
+    def self.head(&block)
+      HeadContext.new(self).instance_eval(&block)
+    end
 
-      def body(&block)
-        self.body_proc = block
-      end
+    # Method to process +body+ statement in +.glue+ templates.
+    #
+    # @yield block which will be executed in context of
+    #   {AudioGlue::Template::BodyContext} object. It should define body of
+    #   audio template.
+    # @return [void]
+    def self.body(&block)
+      self.body_proc = block
+    end
 
-      def inspect
-        if self.name
-          super
-        else
-          info = "<AudioGlue::Template(class)"
-          info << " path=#{path.inspect}" if path
-          info << ">"
-        end
+    # Redefine +inspect+ method to provide more information when it's an
+    # anonymous class.
+    #
+    # @return [String]
+    def self.inspect
+      if self.name
+        super
+      else
+        info = "<AudioGlue::Template(class)"
+        info << " path=#{path.inspect}" if path
+        info << ">"
       end
     end
 
 
+    # @params variables [Hash] hash of parameters which can be used as instance
+    #   variables in +body+ statement of +.glue+ template.
     def initialize(variables = {})
       @variables = variables
     end
 
+    # Executes body of template to build a snippet packet.
+    #
+    # @return [AudioGlue::SnippetPacket]
     def build_snippet_packet
       SnippetPacket.new(format, rate, channels).tap do |packet|
         body = BodyContext.new(packet, @variables)
@@ -37,11 +58,17 @@ module AudioGlue
       end
     end
 
+    # Redefine +inspect+ to provide more information if class of the template
+    # object is anonymous.
+    #
+    # @return [String]
     def inspect
       if self.class.name
         super
       else
-        "<AudioGlue::Template(instance)"
+        info = "<AudioGlue::Template"
+        info << "(path=#{path.inspect})" if path
+        info << " @variables=#{@variables.inspect}>"
       end
     end
   end
--- spec/lib/audio_glue/template_spec.rb
+++ spec/lib/audio_glue/template_spec.rb
@@ -1,4 +1,68 @@
 require 'spec_helper'
 
 describe AudioGlue::Template do
+
+  describe '.inspect' do
+    context 'class has a name' do
+      before do
+        class TestTemplate < AudioGlue::Template
+        end
+      end
+
+      after do
+        Object.send(:remove_const, :TestTemplate)
+      end
+
+      it 'should inspect with standard mechanism' do
+        TestTemplate.inspect.should == "TestTemplate"
+      end
+    end
+
+    context 'anonymous class' do
+      it 'should show AudioGlue::Template' do
+        template = Class.new(AudioGlue::Template)
+        template.inspect.should == "<AudioGlue::Template(class)>"
+      end
+
+      it 'should show path if it presents' do
+        template = Class.new(AudioGlue::Template)
+        template.path = '/path/to/template.glue'
+        template.inspect.should == '<AudioGlue::Template(class) path="/path/to/template.glue">'
+      end
+    end
+  end
+
+
+  describe '#inspect' do
+    context' class has a name' do
+      before do
+        class TestTemplate < AudioGlue::Template
+        end
+      end
+
+      after do
+        Object.send(:remove_const, :TestTemplate)
+      end
+
+      it 'should inspect with standard mechanism' do
+        template = TestTemplate.new(:a => 12)
+        template.inspect.should include('TestTemplate')
+        template.inspect.should include('@variables={:a=>12}')
+      end
+    end
+
+    context 'anonymous class' do
+      it 'should contain AudioGlue::Template' do
+        template = Class.new(AudioGlue::Template).new
+        template.inspect.should == "<AudioGlue::Template @variables={}>"
+      end
+
+      it 'should show path if it presents' do
+        template_class = Class.new(AudioGlue::Template)
+        template_class.path = "/oh.glue"
+        template = template_class.new(:g => 9.8)
+        template.inspect.should == '<AudioGlue::Template(path="/oh.glue") @variables={:g=>9.8}>'
+      end
+    end
+  end
 end
