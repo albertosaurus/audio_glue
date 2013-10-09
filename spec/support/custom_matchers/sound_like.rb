bd02857843d7edc6eeafb293a3933949433ebfaf
baburdick <github.com@bruceburdick.com>
2013-10-09 01:54:37 -0500

    Extract long shell command to string, use String#%; Revise RDoc, formatting
    Task 12148
    Task 12465

--- spec/support/custom_matchers/sound_like.rb
+++ spec/support/custom_matchers/sound_like.rb
@@ -1,5 +1,5 @@
-# Compare sound of two audio files. Based on Chromaprint library and +sox+
-# command like tool.
+# Compare the sound of two audio files.
+# Based on the Chromaprint library and the +sox+ command like tool.
 #
 # @example
 #   "/Airborne.mp3".should sound_like "/ACDC.mp3"
@@ -11,16 +11,19 @@ RSpec::Matchers.define :sound_like do |expected_file|
     threshold = 0.95
 
     if File.exists?(expected_file) && File.exists?(file)
-      # Convert input files into raw 16-bit signed audio (WAV) to process with Chromaprint
-      expected_audio = %x"sox #{expected_file} -e signed -b 16 -t wav - rate #{rate} channels #{channels} 2> /dev/null"
-      audio =          %x"sox #{file}          -e signed -b 16 -t wav - rate #{rate} channels #{channels} 2> /dev/null"
+      # Convert input files into raw 16-bit signed audio (WAV) to process
+      # with Chromaprint:
+      sox_command    = "sox %s -e signed -b 16 -t wav - " \
+                       "rate #{rate} channels #{channels} 2> /dev/null"
+      expected_audio = %x"#{sox_command % [expected_file]}"
+      audio          = %x"#{sox_command % [file]}"
 
-      # Get audio fingerprints
+      # Get audio fingerprints:
       chromaprint = Chromaprint::Context.new(rate, channels)
       expected_fp = chromaprint.get_fingerprint(expected_audio)
-      fp = chromaprint.get_fingerprint(audio)
+      fp          = chromaprint.get_fingerprint(audio)
 
-      # Compare fingerprints and compare result against threshold
+      # Compare fingerprints and compare result against threshold:
       expected_fp.compare(fp) > threshold
     else
       false
